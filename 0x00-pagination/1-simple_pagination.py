e35121831fb08f09a05e62609d74d76172b7428b
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-08-16 13:25:56 +0000

    1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,31 +1,43 @@
 #!/usr/bin/env python3
-"""Simple pagination sample.
+
 """
+This module implements a simple pagination class.
+"""
+
 import csv
+import math
 from typing import List, Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Retrieves the index range from a given page and page size.
-    """
-    start = (page - 1) * page_size
-    end = start + page_size
-    return (start, end)
-
-
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """Initializes a new Server instance.
-        """
         self.__dataset = None
 
-    def dataset(self) -> List[List]:
-        """Cached dataset
+    @staticmethod
+    def index_range(
+        page: int,
+        page_size: int
+    ) -> Tuple[int, int]:
         """
+        Returns a tuple of size two containing a start index and an end index
+
+        Args:
+            page (int): The current page number (1-indexed)
+            page_size (int): The number of items per page
+
+        Returns:
+            Tuple[int, int]: A tuple containing the start and end index
+        """
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+
+        return start_index, end_index
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -35,12 +47,25 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Retrieves a page of data.
         """
-        assert type(page) == int and type(page_size) == int
-        assert page > 0 and page_size > 0
-        start, end = index_range(page, page_size)
-        data = self.dataset()
-        if start > len(data):
+        Returns the page with the given page number and page size.
+
+        Args:
+            page (int, optional): The current page number (1-indexed).
+            Defaults to 1.
+            page_size (int, optional): The number of items per page.
+            Defaults to 10.
+
+        Returns:
+            List[List]: The page with the given page number and page size.
+        """
+        assert all(isinstance(i, int) for i in (page, page_size))
+        assert all(i > 0 for i in (page, page_size))
+
+        dataset = self.dataset()
+
+        start_index, end_index = self.index_range(page, page_size)
+        try:
+            return dataset[start_index:end_index]
+        except IndexError:
             return []
-        return data[start:en]
